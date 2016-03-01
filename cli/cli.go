7345dc34e92e7a7e091e112352d26801685d2a33
Viktor Benei <viktor.benei@gmail.com>
2016-03-01 15:53:47 +0100

    v0.9.7

--- cli/cli.go
+++ cli/cli.go
@@ -56,7 +56,7 @@ func Run() {
 	app := cli.NewApp()
 	app.Name = path.Base(os.Args[0])
 	app.Usage = "bitrise-machine"
-	app.Version = "0.9.6"
+	app.Version = "0.9.7"
 
 	app.Author = ""
 	app.Email = ""
